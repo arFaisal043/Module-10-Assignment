775c3b651809b1d35cde9d9943ccd5213cbeb4f6
arFaisal043 <arfaisal463@gmail.com>
2024-06-25 01:05:49 +0600

    second commit

--- index.js
+++ index.js
@@ -3,4 +3,22 @@
 const calculateSum = ( a , b ) => {
     return  a + b;
 }
-console.log(calculateSum( 4 , 6));
+console.log( calculateSum( 4 , 6) );
+
+
+// 2) Write a function named ‘isEven’ that takes one argument and returns true if the number is even, and false otherwise.
+
+
+const isEven = (n) => {
+
+    //let n = 4;
+
+    if( n % 2 === 0 ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log( isEven( 50 ) );
