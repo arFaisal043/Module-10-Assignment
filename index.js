90dc2d546d421401663e3af95fa2e36f0fe2a215
arFaisal043 <arfaisal463@gmail.com>
2024-06-25 00:58:47 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// 1) Write a function named ‘calculateSum’ that takes two arguments and returns their sum.
+
+const calculateSum = ( a , b ) => {
+    return  a + b;
+}
+console.log(calculateSum( 4 , 6));
